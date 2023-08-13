51c44069bda2a037a08961582632af38ee57f5ce
dmohle <dmohle@gmail.com>
2023-08-13 15:43:50 -0700

    last for today...try this commit

--- myNewCppFile.cpp
+++ myNewCppFile.cpp
@@ -5,5 +5,6 @@ using namespace std;
 int main() {
     cout << "\n\n Welcome to Command Line Practice \n\n";
     cout << "This is a new line";
+    cout << "\n\n and this is the last line! \n\n";
     return 0;
 }
